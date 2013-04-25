c573f0072a6ed4acd094137ee20fa4c0b462416a
murathaksal <murat.haksal@gmail.com>
2013-04-25 22:51:57 +0300

    project is initialized

---
+++ NewIdea.Model/New.cs
@@ -0,0 +1,22 @@
+﻿using ServiceStack.ServiceHost;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewIdea.Model
+{
+    [Route("/Ideas/New")]
+    public class New
+    {
+        public string Idea { get; set; }
+        public string[] Tags { get; set; }
+        public DateTime CreationDate { get; set; }
+    }
+
+    public class NewResponse
+    {
+        public bool Success { get; set; }
+    }
+}
---
+++ NewIdea.Service/IdeaService.cs
@@ -0,0 +1,32 @@
+﻿using NewIdea.Model;
+using ServiceStack.OrmLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ServiceBase = ServiceStack.ServiceInterface.Service;
+
+namespace NewIdea.Service
+{
+    public class IdeaService : ServiceBase
+    {
+        private readonly IDbConnectionFactory _dbSessionFactory;
+
+        public IdeaService(IDbConnectionFactory dbSessionFactory)
+        {
+            _dbSessionFactory = dbSessionFactory;
+        }
+
+        public object Post(New @new)
+        {
+            using(var session = _dbSessionFactory.OpenDbConnection())
+            {
+                session.Insert(@new);
+            }
+
+            return new NewResponse { Success = true };
+        }
+
+    }
+}
